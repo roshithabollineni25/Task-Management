8d83036cd8799bdf009e330fe322e3c3871d4260
roshithabollineni25 <roshithabollineni25@gmail.com>
2026-05-29 09:53:24 +0530

    Add new route imports for payment, map, and AI

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from config.database import init_db
 from Routes.auth_routes import auth_bp
 from Routes.task_routes import task_bp
+from routes.payment_routes import payment_bp
+from routes.map_routes import map_bp
+from routes.ai_routes import ai_bp
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
